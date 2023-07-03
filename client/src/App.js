26a2425b5bb80764f18b5b5b85c6fae768636c71
S Worthington <104519782+StephanieWorth@users.noreply.github.com>
2023-07-03 03:43:59 +0100

    added Register.js and Login.js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import Login from './pages/Login';
 import ProductItem from './pages/ProductItem';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App()  {
   return (
     <div>
-      <ProductItem />
+      <Login />
     </div>
   )
 }
